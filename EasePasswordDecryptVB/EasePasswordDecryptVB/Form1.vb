5f6bf120130b53f4a9e9c46aa4a3b4b8896551d4
mattpotterease <matt.potter@easeinc.com>
2017-12-08 13:22:11 -0800

    can select a database to connect from combobox

--- EasePasswordDecryptVB/EasePasswordDecryptVB/Form1.vb
+++ EasePasswordDecryptVB/EasePasswordDecryptVB/Form1.vb
@@ -1,6 +1,11 @@
 ﻿Imports System.Data.SqlClient
 Public Class Form1
-    Private Sub btnDecrypt_Click(sender As Object, e As EventArgs) Handles btnDecrypt.Click
+
+    Dim ServerName
+    Dim UserName
+    Dim Password
+
+    Public Sub btnDecrypt_Click(sender As Object, e As EventArgs) Handles btnDecrypt.Click
         Dim password As String
         Dim decrypt As String
 
@@ -12,30 +17,41 @@ Public Class Form1
 
     End Sub
 
-    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
-        Dim ServerName
-        Dim UserName
-        Dim Password
+    Public Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
 
         ServerName = txtServerName.Text
         UserName = txtUserName.Text
         Password = txtPassword.Text
 
+
+
+        Dim con As New SqlConnection()
+        con.ConnectionString = "Data Source=" & ServerName & ";uid=" & UserName & ";pwd=" & Password & ";database=aml_prod"
+        con.Open()
+        Dim da As New SqlDataAdapter("Select * From sys.databases order by name", con)
+        Dim table As New DataTable()
+        da.Fill(table)
+        cmb_Databases.DataSource = New BindingSource(table, Nothing)
+        cmb_Databases.DisplayMember = "Name"
+        cmb_Databases.ValueMember = "database_id"
+
+    End Sub
+
+    Public Sub cmb_Databases_SelectionChange(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmb_Databases.SelectionChangeCommitted
+        Dim selectedItem As Object
+        selectedItem = cmb_Databases.GetItemText(cmb_Databases.SelectedItem)
+
+        'MessageBox.Show("Selected Item Text: " & selectedItem.ToString())
+
         Dim str As String = "Data Source=" & ServerName & ";uid=" & UserName & ";pwd=" & Password & ";database=aml_prod"
         Dim con As New SqlConnection(str)
-        Dim com1 As String = "Select userid, username, passwordx From euser"
+        Dim com1 As String = "Select userid, username, passwordx From " & selectedItem.ToString() & ".dbo.euser"
 
-        'Dim Adpt As New SqlDataAdapter(com1, con)
-        'Dim ds As New DataSet()
-        'Adpt.Fill(ds, "Emp")
-        'DataGridView1.DataSource = ds.Tables(0)
-
-        Dim ds2 As New DataSet()
-        Dim com2 As String = "Select name From sys.databases"
-        Dim Adpt2 As New SqlDataAdapter(com2, con)
-        Adpt2.Fill(ds2, "sys.databases")
-        cmb_Databases.DataSource = ds2
-        cmb_Databases.DisplayMember = "name"
+        Dim Adpt As New SqlDataAdapter(com1, con)
+        Dim ds As New DataSet()
+        Adpt.Fill(ds, "Emp")
+        DataGridView1.DataSource = ds.Tables(0)
 
     End Sub
 
